1186e9fa748ba8c97f9bdba4aa07ab77a8afc42d
madobon <mimicopy@live.jp>
2014-03-05 15:04:26 +0900

    direct_to_template(deprecated in django 1.3) -> TemplateView
    https://docs.djangoproject.com/en/dev/topics/class-based-views/

--- django_allauth_sample/src/django_allauth_sample/urls.py
+++ django_allauth_sample/src/django_allauth_sample/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib.auth.decorators import login_required
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,5 +18,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^$', login_required(direct_to_template), {'template': 'index.html'}),
+    url(r'^$', login_required(TemplateView.as_view(template_name='index.html'))),
     )
